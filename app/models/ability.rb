6d2e94c1b9705fe613f334dff425a70c18d24151
Alaina Hardie <alaina@precisionnutrition.com>
2018-11-28 10:33:23 -0700

    Update header and footer
    - Control display of menu items
    - Complete sticky footer implementation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,23 +19,28 @@ class Ability
 
   def admin
     can :manage, :all
-        can :administer_users, :all
-        can :leaderboard, :all
+    can :administer_users, :all
+    can :administer_action_steps, :all
+    can :leaderboard, :all
+    can :check_in, :all
+    can :show_login_header, :all
   end
 
   def coach
+    can :show_login_header, :all
   end
 
   def check_in
-        can [:update, :complete, :read, :open], UserActionStep
-        can :complete, UserActionStep
-        can :read, User
+    can [:update, :complete, :read, :open], UserActionStep
+    can :complete, UserActionStep
+    can :read, User
+    can :check_in, :all
   end
 
   def leaderboard
     can :leaderboard, :all
-        can :read, UserActionStep
-        can :read, User
+    can :read, UserActionStep
+    can :read, User
   end
 
   def client
