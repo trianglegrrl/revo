4be4f9ee74e01c9b900039bbd9fae51857760730
Alaina Hardie <alaina@precisionnutrition.com>
2018-11-28 11:24:27 -0700

    Add sign in redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
   end
+
+  def after_sign_in_path_for(resource)
+    if(current_user.admin?)
+      "/users"
+    elsif(current_user.check_in?)
+      "/check_in"
+    elsif(current_user.leaderboard?)
+      "/leaderboard"
+    elsif(current_user.coach?)
+      "/users/edit"
+    else
+      "/users/edit"
+    end
+  end
 end
