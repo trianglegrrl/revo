15e98fac453a21477b3b3456708f03f3e42616b1
Alaina Hardie <alaina@precisionnutrition.com>
2018-11-29 17:26:15 -0700

    Make Scheduling Cards Great Again

--- app/models/default_user_action_step.rb
+++ app/models/default_user_action_step.rb
@@ -2,4 +2,12 @@ class DefaultUserActionStep < ApplicationRecord
   belongs_to :user
 
   serialize :action_step_ids, Array
+
+  scope :current, -> {
+    where('starting_at < ?', Time.now).order(starting_at: :desc).limit(1)
+  }
+
+  scope :future, -> {
+    where('starting_at > ?', Time.now).order(:starting_at)
+  }
 end
